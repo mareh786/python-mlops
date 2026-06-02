f7844ce9fc3720215ffefd191863c95417da2d1b
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-06-02 22:13:54 +0530

    Add basic operations and data types in Python

---
+++ python-basics/datatypes.py
@@ -0,0 +1,33 @@
+print("Start small. Ship something.") 
+
+# Starting with Data Types 
+print("Planning to practice python data types in Python!!") 
+
+num1 = 12 
+num2 = 15 
+print(f"First number: {num1}") 
+print(f"Second number: {num2}") 
+print(f"Sum of numbers {num1+num2}") 
+print(f"Diff of numbers {num1-num2}") 
+print(f"Product of numbers {num1*num2}") 
+print(f"Div of numbers {num1/num2}") 
+print(f"Modulo of numbers {num1%num2}") 
+print(f"Power of numbers {num1**num2}") 
+
+#Boolean 
+raining= True 
+sun_is_shining= 0 
+print(f"Is it raining :{raining}") 
+print(f"Is sun up : {bool(sun_is_shining)}") 
+
+# String 
+text1= "Allah " 
+text2= "Is " 
+text3= "Greatest" 
+
+full_text= text1+text2+text3 
+print(f"The only truth is : {full_text}") 
+
+print(f"The only creator is {full_text[0:5]}") 
+
+print(f"Allah is {full_text[9:]}")
