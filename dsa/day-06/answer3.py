7ac9bafd09477de6346a6a2c3784b7835bb03afe
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-02 19:24:31 +0530

    Added answer 3

---
+++ dsa/day-06/answer3.py
@@ -0,0 +1,8 @@
+# Problem 3: Check uniqueness of Elements in list
+
+def allUnique(nums):
+    isUnique = False
+    if len.nums == len.set(nums):
+        isUnique = True
+    return isUnique
+print(allUnique([1, 2, 3, 4, 5]))
