0eaa3a00b2ed12ef3a87ee1755208a364f167d96
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-02 18:57:46 +0530

    Corrected

--- dsa/day-06/answer1.py
+++ dsa/day-06/answer1.py
@@ -2,7 +2,6 @@ def searchFunc(nums, target):
     for i in nums:
         if i == target:
             return nums.index(i)
-        else:
-            return -1
+        return -1
 
 print(searchFunc([1, 2, 3, 4, 5], 5))
