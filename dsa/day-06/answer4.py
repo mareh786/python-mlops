02b69d2be13476aee40e2b5dc61caf1d2a7c918e
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-02 19:39:41 +0530

    Added answer 4

---
+++ dsa/day-06/answer4.py
@@ -0,0 +1,15 @@
+# Problem 4: Finding common elements in two lists
+
+def commonChar(lst1, lst2):
+    commonPresent = False
+    for char in lst1:
+        if char in lst2:
+            commonPresent == True
+    return commonPresent
+
+value = commonChar([1, 2, 3, 4], [1, 9, 7, 5])
+
+if value == True:
+    print("Common element exists")
+else: 
+    print("No common elements")
