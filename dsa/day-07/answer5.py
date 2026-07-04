98085b7b7e1973420b9f187d12d4b9de7e404a15
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-04 20:03:47 +0530

    Trying with functions

--- dsa/day-07/answer5.py
+++ dsa/day-07/answer5.py
@@ -6,3 +6,10 @@ for i in range(1, n+1):
         print(num, end=" ")
         num+=1
     print()
+
+def floyd_triangle(first_num, rows):
+    for i in range(i, rows+1):
+        for j in range(1, i+1):
+            print(first_num, end= " ")
+        print()
+floyd_triangle(10, 5)
