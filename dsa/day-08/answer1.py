45a801f8eed1b9d3a0e4dadf5f4140deccc110a1
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-05 19:49:02 +0530

    Added answer 1

---
+++ dsa/day-08/answer1.py
@@ -0,0 +1,11 @@
+# Find Most frequent Element
+lst= [1,2,3,4,5,5,4,4,3,3,3,2,1]
+
+freq = {}
+for ch in lst: 
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
+
+print(freq)
