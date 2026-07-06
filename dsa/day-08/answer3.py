6b6694f7d2f93d2f3db441b16425c77d992088ab
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-06 16:23:00 +0530

    Implement intersection function for two lists
    Added two approaches to find the intersection of two lists.

--- dsa/day-08/answer3.py
+++ dsa/day-08/answer3.py
@@ -1,1 +1,17 @@
-# Find the intersection of a List
+# Find the intersection of two lists
+# Approach 1
+def intersect(lst1, lst2):
+ intersection= list(set(lst1) & set(lst2))
+ return intersection
+
+print(intersect([1,2,3,4,5], [3,4,5,6,7]))
+
+# Approach 2
+def inter(lst1, lst2):
+    final = []
+    for ch in lst1:
+        if ch in lst2:
+            final.append(ch)
+    return final
+
+print(inter(["a","b","c"], ["c", "d", "a"]))
