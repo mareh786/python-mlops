30120a025a31626e6ce08085cebf28ddf0f63ad6
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-06 16:04:15 +0530

    Fix formatting and indentation in rem_duplicate function

--- dsa/day-08/answer2.py
+++ dsa/day-08/answer2.py
@@ -1,1 +1,9 @@
 # Remove Duplicate characters from a String
+
+def rem_duplicate(str): 
+ new_str = "" 
+ for ch in str: 
+ if ch not in new_str: 
+ new_str = new_str + ch 
+ return new_str 
+print(rem_duplicate("programming"))
