cc5b57bef477a52ebf906cc413078844aefaaf79
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-07 17:21:04 +0530

    Add function to find the longest word in a sentence

--- dsa/day-10/answer1.py
+++ dsa/day-10/answer1.py
@@ -1,1 +1,19 @@
 # Print the longest word in a sentence
+def str_to_lst(sentence):
+    lst = sentence.split()
+
+    if len(lst) == 1:
+        return lst[0]
+
+    count_lst = []
+    for word in lst:
+        count_lst.append(len(word))
+
+    longest_word_length = max(count_lst)
+
+    for word in lst:
+        if len(word) == longest_word_length:
+            return word
+
+value = str_to_lst("Python is an amazingg Language")
+print(f"The longest word in the string is: {value}")
