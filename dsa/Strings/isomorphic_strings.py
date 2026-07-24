59f1366df0a346d77546af2f801f97d7a0ba7ed1
Mohammed Adil Rehan <116783167+mareh786@users.noreply.github.com>
2026-07-24 22:03:11 +0530

    Implement is_isomorphic function for string comparison
    Added a function to check if two strings are isomorphic.

--- dsa/Strings/isomorphic_strings.py
+++ dsa/Strings/isomorphic_strings.py
@@ -1,1 +1,8 @@
+# Find out if the strings are isomorphic
 
+def is_isomorphic(str1,str2)->bool:
+    if len(str1)!=len(str2):
+        return "can't be isomorphic"
+    
+        
+print(is_isomorphic("aad", "abcd"))
